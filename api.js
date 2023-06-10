98c7d8640246212ffc6db42948fc8f00cd430d94
cleargreentrees <135859321+cleargreentrees@users.noreply.github.com>
2023-06-09 23:25:25 -0700

    Update api.js

--- api.js
+++ api.js
@@ -2,6 +2,7 @@
 const { BardAPI } = require('bard-api-node');
 const express = require('express');
 const dotenv = require('dotenv');
+const rateLimit = require('express-rate-limit'); // Import the express-rate-limit module
 
 // Load environment variables
 dotenv.config();
@@ -14,6 +15,19 @@ const app = express();
 
 // Set the port number
 const port = process.env.PORT || 3000;
+
+// Define a rate limit middleware
+const limiter = rateLimit({
+  windowMs: 3 * 1000, // 3 seconds
+  max: 10, // limit each IP to 10 requests per windowMs
+  handler: (req, res) => {
+    // Send a too many requests response
+    res.status(429).send('Too many requests, please try again later');
+  },
+});
+
+// Apply the rate limit middleware to all routes
+app.use(limiter);
 
 // Define a route handler for GET requests to the root path
 app.get('/', async (req, res) => {
@@ -29,10 +43,20 @@ app.get('/', async (req, res) => {
     }
 
     // Set the session information for authentication
-    await bard.setSession('__Secure-1PSID', process.env.BARD_TOKEN);
+    await bard.setSession('__Secure-1PSID', process.env.BARDTOKEN);
+
+    // Set a timeout for the request
+    const timeout = setTimeout(() => {
+      // Abort the request and send a timeout response
+      bard.abort();
+      res.status(408).send('Request timed out');
+    }, 10 * 1000); // 10 seconds
 
     // Send the input query to Bard and get the response
     const response = await bard.getBardResponse(input);
+
+    // Clear the timeout
+    clearTimeout(timeout);
 
     // Send the response content as JSON
     res.json({ content: response.content });
